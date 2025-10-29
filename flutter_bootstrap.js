644c5c5ca2bd8ca28c8ae478be9e0ff6c841ca5c
yugaldhiman <yugaldhiman14@gmail.com>
2025-10-29 23:12:56 +0530

    Built web
    Branch: main
    Commit: ccf09fb16e400da0ec3a4f9bd44e4030c81dc4ae
    
    package:peanut 6.0.0

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"035316565ad77281a75305515e4682e6c4c6f7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "593574741"
+    serviceWorkerVersion: "1575581812"
   }
 });
